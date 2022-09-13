cc2f293e20f6cab31195025c5ca23604b3a990c6
fayaaz226 <113225098+fayaaz226@users.noreply.github.com>
2022-09-13 09:13:50 +0530

    Update to print right triangle star pattern.cpp

--- to print right triangle star pattern.cpp
+++ to print right triangle star pattern.cpp
@@ -1,16 +1,26 @@
 #include<iostream>
 using namespace std;
+
 int main()
 {
-int i,j,n;
-cout<<"enter the number of rows => ";
-cin>>n;
-for(i=0;i<n+1;i++)
-{
-for(j=0;j<i+1;j++)
-{
-cout<<"*";
-}
-cout<<"\n";
-}
+	int i, j, k, rows;
+     
+    cout << "Enter Mirrored Right Triangle Star Pattern Row = ";
+    cin >> rows;
+
+    cout << "Mirrored Right Angled Triangle Star Pattern\n"; 
+
+    for(i = 1; i <= rows; i++)
+    {
+    	for(j = 0; j <= rows - i; j++)
+		{
+            cout << " ";
+        }
+        for(k = 0; k < i; k++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }		
+ 	return 0;
 }
